da572512da09b7a99d8d799eecbc92cb012ac991
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-07-12 01:50:25 -0400

    first section is much better

--- src/homepage/homepage.jsx
+++ src/homepage/homepage.jsx
@@ -6,17 +6,19 @@ export default function Homepage() {
     console.log(BackgroundImage);
     //  absolute inset-x-20 inset-y-80 center
     //  p-60 pl-10 text-6xl font-bold flex-row
+    // "text-2xl my-10 grid grid-cols-2 gap-4 rows-start-5"
+    //'grid grid-cols-2 mb-4 col-span-4 rows-span-4'
     return (
         <div>
-            <div style={{backgroundImage: `url('${BackgroundImage}')`}} className=" bg-cover h-screen bg-center bg-cover bg-relative bg-local bg-no-repeat ">
-                <div class="font-mono text-justify text-6xl grid grid-cols-8 grid-rows-8">
-                    <div className='grid grid-cols-2 mb-4 col-span-4 rows-span-4'>
-                        <p class='text-green-800 col-span-2 object-fill mb-10'>TRANSFORM A LIFE <span className='w-full'/></p>
+            <div style={{backgroundImage: `linear-gradient(to right, black 0%, rgba(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.5) 60%, transparent 100%),url('${BackgroundImage}')`}} className=" bg-cover h-screen bg-center bg-cover bg-relative bg-local bg-no-repeat ">
+                <div class="ml-20 font-mono font-bold text-6xl grid grid-cols-6 grid-rows-6 justify-items-stretch">
+                    <div className='col-start-1 col-end-4 row-start-3 text-justify'>
+                        <p class='text-green-800 col-span-2 object-fill mb-10'>TRANSFORM A LIFE </p>
                         <p class='text-white col-span-2'>WITH TECHNOLOGY</p>
                     </div>
-                    <div text className="text-2xl my-10 grid grid-cols-2 gap-4 rows-start-5">
-                        <Link className="bg-green-500 rounded">🤍 Donate your ewaste</Link>
-                        <Link> Discover our programs </Link>
+                    <div className='mt-10 w-81 grid grid-cols-2 col-start-1 col-end-4 row-start-4 text-center text-xl gap-3'>
+                        <Link className="bg-green-500 h-10 rounded">🤍 Donate your ewaste</Link>
+                        <Link className='border-2 border-white text-white h-10'> Discover our programs </Link>
                     </div>
                 </div>
             </div>
@@ -45,7 +47,7 @@ export default function Homepage() {
 
                         </div> */}
                     </div>
-                    <div className='grid grid-cols-2 col-start-4 col-end-6 row-start-3 text-center text-xl'>
+                    <div className='grid grid-cols-2 col-start-4 col-end-6 row-start-3 text-center text-xl gap-3'>
                             <Link className='bg-[#0FE006] rounded h-14 pr-3 pt-3'>🤍 Donate your ewaste</Link>
                             <Link className='border-2 border-black rounded h-14 pr-3 pt-3'>Become a Partner</Link>
                     </div>
