d81e737407ab6535fc878a2baff9bc76d3ac2f02
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-07-17 16:51:35 -0400

    refurbishment skeleton

--- src/homepage/refurbished.jsx
+++ src/homepage/refurbished.jsx
@@ -20,6 +20,43 @@ export default function Refurbished() {
                     </div>
                 </div>
             </div>
+            <div>
+                <h2>
+                    Supporing Underserved Communities with free computers
+                </h2>
+                <p>Forty-one percent of adults with lower incomes do not own a desktop or laptop computer,
+                    limiting their access to economic, educational, and social opportunities.
+                    Computers4People provides refurbished laptops, desktops, all-in-ones, and tablets at
+                    no cost to individuals and organizations in need to help people grasp the resources
+                    at their disposal.
+                </p>
+            </div>
+            <div>
+                <h2>Our Devices come from Generous Donors</h2>
+                <p>Our devices come from individuals and organizations who generously
+                    provide their unused equipment to us. Each device undergoes a meticulous
+                    refurbishing and data wiping process to be in optimal condition before being
+                    matched to a recipient. Due to our reliance on donated devices, there may be
+                    a waiting period for computer applicants.
+                </p>
+            </div>
+            <div>
+                <h2>Giving Devices a Second Chance</h2>
+                <p>Each device has a unique story before reaching its new home.</p>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                <h2>About our devices</h2>
+                <p>Our computers and tablets are digital tools in an optimal condition.</p>
+                <div className="mt-4 row-start-3 col-span-3 grid grid-cols-3 justify-items-stretch gap-28 h-80">
+                        <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold pb-5"> <img className='mb-10'src="../maps.png" />BECOME AN EWASTE DROP-OFF SITE</Link>
+                        <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold pb-5"> <img className="mb-10" src="../handshake.png"></img>SPONSOR DIGITAL ACCESS</Link>
+                        <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold pb-5"> <img className='mb-10' src='../hands.png'></img>VOLUNTEER</Link>
+                    </div>
+            </div>
+            
         </div>
     )
 }
