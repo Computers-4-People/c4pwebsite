a17d3e97c17bf0d05a0357d69586b81d730f04c1
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-08-01 16:03:08 -0400

    fixed issues with carosel

--- src/components/carosel/carosel.jsx
+++ src/components/carosel/carosel.jsx
@@ -17,8 +17,10 @@ export default function Carosel ({ blocks }, interval = 0) {
         <div className='overflow-hidden whitespace-nowrap mx-auto relative' onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             {/* <button onClick={prevSlide}></button> */}
             <span className='inline-block inline-flex space-x-5 pl-[100%] mb-14' style={{animation: `ticker 10s linear infinite ${isHovered ? 'paused' : ''}`}}>
-                <div className='h-64 w-64 bg-gray-300 rounded-xl'>This is a test</div>
-                <div className='h-64 w-64 bg-gray-300 rounded-xl'>This is a test</div>
+                <div className='h-64 w-auto bg-gray-300 rounded-xl'>This is a test</div>
+                <div className='h-64 w-auto bg-gray-300 rounded-xl'>This is a test</div>
+                <div className='h-64 w-auto bg-gray-300 rounded-xl'>This is a test</div>
+                <div className='h-64 w-auto bg-gray-300 rounded-xl'>This is a test</div>
             </span>
             <style>
                 {`
