598fd58d4aa0d06078a0bf61b8308c29bd4e2c27
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-08-01 13:47:30 -0400

    made the carosel pause upon hover

--- src/components/carosel/carosel.jsx
+++ src/components/carosel/carosel.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 export default function Carosel ({ blocks }, interval = 0) {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
     const nextSlide = () => { 
         setCurrentIndex((prevIndex) => 
             prevIndex === blocks.length -1 ? 0 : prevIndex + 1
@@ -13,9 +14,9 @@ export default function Carosel ({ blocks }, interval = 0) {
     }
 
     return (
-        <div className='overflow-hidden whitespace-nowrap mx-auto relative'>
+        <div className='overflow-hidden whitespace-nowrap mx-auto relative' onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             {/* <button onClick={prevSlide}></button> */}
-            <span className='inline-block inline-flex space-x-5 pl-[100%] mb-14' style={{animation: 'ticker 20s linear infinite'}}>
+            <span className='inline-block inline-flex space-x-5 pl-[100%] mb-14' style={{animation: `ticker 10s linear infinite ${isHovered ? 'paused' : ''}`}}>
                 <div className='h-64 w-64 bg-gray-300 rounded-xl'>This is a test</div>
                 <div className='h-64 w-64 bg-gray-300 rounded-xl'>This is a test</div>
             </span>
@@ -24,6 +25,9 @@ export default function Carosel ({ blocks }, interval = 0) {
                     @keyframes ticker{
                         0% {transform: translateX(0);}
                         100% {transform: translateX(-100%);}
+                    }
+                    span:hover {
+                        animation-play-state: paused;
                     }
                 `}
             </style>
