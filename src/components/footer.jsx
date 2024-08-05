3077431bc89d08776707e9f184b48ae3a67e758a
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-08-05 16:40:32 -0400

    testing footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,17 +1,79 @@
 import { Link } from "react-router-dom"
 
 export default function Footer () {
-    return(<div class="footer static bottom-0 w-full text-white bg-gray-900 grid grid-cols-4 h-64">
-        <div className='grid gaps-4'>
-            <div class='font-bold text-l'>Computers 4 People</div>
+    return(
+    // <div class="footer static bottom-0 w-full text-white bg-gray-900 grid grid-cols-4 h-96 p-10 text-justify">
+    //     <div className='grid gap-5'>
+    //         <div class='font-bold text-xl'>Computers 4 People</div>
+    //         <div>About us</div>
+    //         <div>Blog</div>
+    //         <div>Help Center</div>
+    //         <div>Press & Media</div>
+    //         <div>Team</div>
+    //     </div>
+    //     <div className='grid gap-5'>
+    //         <div  class='font-bold text-xl'> Programs & Initiatives</div> 
+    //         <div>
+    //             Refurbished Devices
+    //         </div>
+    //         <div>
+    //             Digital Skills Classes
+    //         </div>
+    //         <div>
+    //             PC Building Courses
+    //         </div>
+    //         <div>
+    //             EWaste Recycling
+    //         </div>
+    //         <div>
+    //             Become a Dropoff Site
+    //         </div>
+    //     </div>
+    //     <div className='grid gaps-4'>
+    //         <div  class='font-bold text-xl'>Support</div>
+    //             <Link>
+    //                 Contact Us
+    //             </Link>
+    //             <Link>
+    //                 Donate a Device
+    //             </Link>
+    //             <Link>
+    //                 <p>Volunteer</p>
+    //             </Link>
+    //             <Link>
+    //                 <p>Careers</p>
+    //             </Link>
+    //             <Link>
+    //                 <p>Make a Financial Contribution</p>
+    //             </Link>
+    //     </div>
+    //     <div className='grid gaps-4'>
+    //         <div  class='font-bold text-xl'>Policies</div>
+    //             <div>
+    //                 Privacy
+    //             </div>
+    //             <div>
+    //                 Device Guarantee
+    //             </div>
+    //             <div>
+    //                 Returns
+    //             </div>
+    //             <div>
+    //                 Shipping
+    //             </div>
+    //         </div>
+    //     </div>
+    <div class="footer static bottom-0 w-full text-white bg-gray-900 grid grid-cols-4 h-96 p-10 text-justify">
+        <div className='grid gap-5'>
+            <div class='font-bold text-xl'>Computers 4 People</div>
             <div>About us</div>
             <div>Blog</div>
             <div>Help Center</div>
             <div>Press & Media</div>
             <div>Team</div>
         </div>
-        <div className='grid gaps-4'>
-            <div  class='font-bold text-l'> Programs & Initiatives</div> 
+        <div className='grid gap-5'>
+            <div  class='font-bold text-xl'> Programs & Initiatives</div> 
             <div>
                 Refurbished Devices
             </div>
@@ -29,7 +91,7 @@ export default function Footer () {
             </div>
         </div>
         <div className='grid gaps-4'>
-            <div  class='font-bold text-l'>Support</div>
+            <div  class='font-bold text-xl'>Support</div>
                 <Link>
                     Contact Us
                 </Link>
@@ -47,7 +109,7 @@ export default function Footer () {
                 </Link>
         </div>
         <div className='grid gaps-4'>
-            <div  class='font-bold text-l'>Policies</div>
+            <div  class='font-bold text-xl'>Policies</div>
                 <div>
                     Privacy
                 </div>
@@ -60,7 +122,7 @@ export default function Footer () {
                 <div>
                     Shipping
                 </div>
-            </div>
         </div>
+    </div>
     )
 }
