89bac56d23838b784d3247a337a653c01dcaa6f7
dylanzajac1 <143460154+dylanzajac1@users.noreply.github.com>
2024-08-10 11:26:29 -0400

    I added lol to title

--- src/homepage/homepage.jsx
+++ src/homepage/homepage.jsx
@@ -14,7 +14,7 @@ export default function Homepage() {
             <div style={{backgroundImage: `linear-gradient(to right, black 0%, rgba(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.5) 60%, transparent 100%),url('${BackgroundImage}')`}} className=" bg-cover h-screen bg-center bg-cover bg-relative bg-local bg-no-repeat ">
                 <div class="ml-20 grid grid-cols-6 grid-rows-6 justify-items-stretch">
                     <div className='col-start-1 col-end-4 row-start-2 text-justify font-title text-8xl'>
-                        <p class='text-[#0FE006] col-span-2 object-fill mb-5'>TRANSFORM A LIFE </p>
+                        <p class='text-[#0FE006] col-span-2 object-fill mb-5'>TRANSFORM A LIFE LOL</p>
                         <p class='text-white col-span-2'>WITH TECHNOLOGY</p>
                     </div>
                     {/* <div className='mt-10 w-81 grid grid-cols-2 col-start-1 col-end-4 row-start-4 text-center text-xl gap-3'>
