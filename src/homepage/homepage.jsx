e7168a16643e2013b0837a7d687916e4f42b3c5e
dylanzajac1 <143460154+dylanzajac1@users.noreply.github.com>
2024-08-10 19:50:50 -0400

    added animations

--- src/homepage/homepage.jsx
+++ src/homepage/homepage.jsx
@@ -48,16 +48,16 @@ export default function Homepage() {
             <div style={{backgroundImage: `linear-gradient(to right, black 0%, rgba(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.5) 60%, transparent 100%),url('/homepage/background.jpeg')`}} className=" bg-cover h-screen bg-center bg-cover bg-relative bg-local bg-no-repeat ">
                 <div class="ml-20 grid grid-cols-6 grid-rows-6 justify-items-stretch">
                     <div className='col-start-1 col-end-4 row-start-2 text-justify font-title text-8xl'>
-                        <p class='text-c4p col-span-2 object-fill mb-5'>TRANSFORM A LIFE</p>
-                        <p class='text-white col-span-2'>WITH TECHNOLOGY</p>
+                        <p class='text-c4p mb-5 animate-fade-up'>TRANSFORM A LIFE</p>
+                        <p class='text-white animate-fade-up'>WITH TECHNOLOGY</p>
                     </div>
                     {/* <div className='mt-10 w-81 grid grid-cols-2 col-start-1 col-end-4 row-start-4 text-center text-xl gap-3'>
                         <Link className="bg-[#0FE006] h-10 rounded">🤍 Donate your ewaste</Link>
                         <Link className='border-2 rounded border-white text-white h-10'> Discover our programs </Link>
                     </div> */}
                     <div className='mt-10 row-start-3 col-span-4 row-span-2 flex flex-row justify-items-start flex-wrap text-center text-xl w-128'>
-                        <Link to="/ewaste" className="bg-c4p h-10 mr-3 h-11 rounded-md pt-2 px-5 hover:bg-c4p-hover hover:text-white">🤍 Donate your ewaste</Link>
-                        <Link to="/programs" className='border-2 rounded-md border-white h-11 text-white h-10 pt-1.5 px-5 hover:bg-white hover:text-black'> Discover our programs </Link>
+                        <Link to="/ewaste" className="animate-fade-up bg-c4p h-10 mr-3 h-11 rounded-md pt-2 px-5 hover:bg-c4p-hover hover:text-white">🤍 Donate your ewaste</Link>
+                        <Link to="/programs" className='animate-fade-up border-2 rounded-md border-white h-11 text-white h-10 pt-1.5 px-5 hover:bg-white hover:text-black'> Discover our programs </Link>
                     </div>
                 </div>
             </div>
@@ -71,12 +71,12 @@ export default function Homepage() {
             <div className='h-screen text-black'>
                 <h2 className="ml-14 mb-20 text-5xl font-bold">Give Unused Tech a Second Chance</h2>
                     <div className='grid grid-rows-6 grid-cols-6 relative'>
-                    <img src="../secondchance.png" className='col-span-3 row-span-3' />
+                    <img src="../secondchance.png" className='col-span-3 row-span-3 animate-fade-right animate-once' />
     
-            <div className='absolute inset-0 col-start-4 col-end-6 row-start-1 row-end-4 bg-contain bg-top bg-no-repeat z-0' style={{ backgroundImage: "url('../quotes.png')" }} />
+            <div className='absolute inset-0 col-start-4 col-end-6 row-start-1 row-end-4 bg-contain bg-top bg-no-repeat z-0 animate-jump animate-once' style={{ backgroundImage: "url('../quotes.png')" }} />
     
             <div className='z-10 col-start-4 col-end-6 row-start-2 row-end-2 text-center text-xl bg-contain bg-top bg-no-repeat'>
-                <p>Thanks to the [refurbished] laptop I received from Computers 4 People; I can now create artwork every night when I get home!</p>
+                <p>Thanks to the laptop I received from Computers 4 People; I can now create artwork every night when I get home!</p>
                     <p className='text-right italic'>-Mallika</p>
             </div>
     
