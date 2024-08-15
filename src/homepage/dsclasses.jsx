861c8cfadb4423dff299a06c66c1cbc5b4ee50fd
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-08-15 16:31:32 -0400

    added and formated new section to dsclasses

--- src/homepage/dsclasses.jsx
+++ src/homepage/dsclasses.jsx
@@ -5,7 +5,7 @@ export default function DSClasses() {
         <div className='font-sans overflow-x-hidden'>
             <div
                 style={{
-                    backgroundImage: `linear-gradient(to right, black 0%, rgba(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.5) 60%, transparent 100%),url('/socialmedia/socialmediabackground.png')`
+                    backgroundImage: `linear-gradient(to right, black 0%, rgba(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.5) 60%, transparent 100%),url('/Programs/Digital Skills/Digital_Skills_Classes_Community_Access.jpg')`
                 }}
                 className="bg-cover h-screen bg-center bg-fixed bg-no-repeat flex items-center justify-center"
             >
@@ -26,9 +26,27 @@ export default function DSClasses() {
                         </Link>
                     </div>
                 </div>
+                
             </div>
             <div>
+            <div className='h-full text-black py-20'>
+            <div className='grid grid-rows-2 grid-cols-6 relative'>
+                <img src="../Programs/Digital Skills/Refurbished_Computers_Donation_Dylan_Zajac_.png" className=' col-span-2 row-span-3 animate-fade-right animate-once' />
+                <div className='z-10 col-start-3 col-span-4 row-start-1 self-end text-xl bg-top bg-no-repeat pl-12'>
+                    <h3 className='text-5xl font-title'>Collaborating with organizations to offer free digital skills classes to people in need</h3>
+                    <p className='text-2xl'>We collaborate with organizations like RiseBoro, Community Access, Jersey City Housing Authority, and the Hoboken Public Library to deliver
+                        digital literacy courses to under-resourced communities. Our classes are held across New York City and northern New Jersey. If your organization is interested in
+                        hosting a digital skills class, please reach out to us-we'd love to partner with you.
+                    </p>
                 </div>
+        
+                <div className='grid grid-cols-2 col-start-3 col-end-6 row-start-2 self-start mt-4 items-center text-center text-xl gap-3 z-20 px-12'>
+                        <Link to="/ewaste" className='bg-c4p rounded h-12 pr-3 pt-2.5 hover:bg-c4p-hover hover:text-white'>Apply for a computer</Link>
+                        <Link to="/partner" className='border-2 border-black rounded h-12 pr-1 pt-2.5 hover:bg-black hover:text-white'>Enroll in a digital class</Link>
+                        </div>
+                    </div>
+            </div>
+            </div>
         </div>
     )
 }
