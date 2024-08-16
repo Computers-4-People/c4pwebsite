2b6d1d11500490f968d91f38bb5e1d5b42fb5777
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-08-16 16:42:26 -0400

    changed formatting again on homepage testimonial

--- src/components/iconcards.jsx
+++ src/components/iconcards.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function IconCards () {
     return(
-        <div className="grid grid-cols-3 gap-28 h-1/2">
-            <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold pb-5"> <img className='mb-10'src="../maps.png" />BECOME AN EWASTE DROP-OFF SITE</Link>
-            <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold pb-5"> <img className="mb-10" src="../handshake.png"></img>SPONSOR DIGITAL ACCESS</Link>
-            <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold pb-5"> <img className='mb-10' src='../hands.png'></img>VOLUNTEER</Link>
+        <div className="grid grid-cols-3 gap-20 p-5 h-1/2" style={{height:'40vh', width:'90vw'}}>
+            <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold p-5 h-full"> <img className='mb-10'src="../maps.png" />BECOME AN EWASTE DROP-OFF SITE</Link>
+            <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold p-5 h-full"> <img className="mb-10" src="../handshake.png"></img>SPONSOR DIGITAL ACCESS</Link>
+            <Link className="border shadow-2xl flex flex-col justify-end text-center font-bold p-5 h-full"> <img className='mb-10' src='../hands.png'></img>VOLUNTEER</Link>
         </div>
     )
 }
