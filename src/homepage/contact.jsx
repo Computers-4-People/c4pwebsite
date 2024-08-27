28f3acfb7243243e38ccbf94db5487d69c60921e
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-08-27 14:36:00 -0400

    added header to contact page

--- src/homepage/contact.jsx
+++ src/homepage/contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../components/header";
 
 export default function Contact() {
     const scrollToForm = () => {
@@ -11,6 +12,14 @@ export default function Contact() {
 
     return (
         <div className='font-sans overflow-x-hidden'>
+            <Header props={{
+                bgImage: '/contact us/contactbackground.png',
+                titlePart1: 'Contact Us',
+                titlePart2: `We're Here to Assist to You`,
+                description: <div className='space-y-7'><p>Hours: Monday-Friday 9 a.m. to 5p.m EST</p> 
+                <p>Visit our Help Center for instant answers or reach out to us for further assistance</p></div>,
+                links: [{text: 'Contact our Team', clickAction: scrollToForm}, {text: 'Visit our Help Center'}]
+            }}/>
             <div
                 style={{
                     backgroundImage: `linear-gradient(to right, black 0%, rgba(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.5) 60%, transparent 100%),url('/contact us/contactbackground.png')`
