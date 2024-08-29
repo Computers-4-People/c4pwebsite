19cdf482fac434bb626b303fb7e9a1814770e39b
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-08-29 14:38:32 -0400

    added right padding to testimonial text

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -13,13 +13,14 @@ export default function Testimonial(props) {
     // }
     return(
             <div className='bg-cover font-sans px-4 mt-20 mb-20 sm:px-10 md:px-20 my-10'>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 items-center h-full'>
+                {/* <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 items-center h-full  lg:h-[40vh]'> */}
+                <div className={`flex flex-col ${side === 'right' ? 'lg:flex-row-reverse' : 'lg:flex-row'} items-center h-full`}>
                     {/* Image Section */}
-                    <div className={`${side === 'right' ? 'lg:col-start-3' : 'lg:col-start-1'} h-full w-full lg:h-[40vh] lg:w-[40vw]`}   >
-                        <img src={image} alt="" className='h-full w-full object-cover'/>
+                    <div className={`${side === 'right' ? 'lg:col-start-3' : 'lg:col-start-1'} h-full w-full`}   >
+                        <img src={image} alt="" className='h-auto w-auto max-h-full max-w-full object-contain'/>
                     </div>
                     {/* Text Section */}
-                    <div className={`flex flex-col h-3/4 row-start-1 lg:col-span-2 ${side === 'right' ? 'lg:col-start-1 text-justify' : 'lg:col-start-2'}`}>
+                    <div className={`flex flex-col w-full row-start-1 lg:col-span-2 ${side === 'right' ? 'lg:col-start-1 pr-5' : 'lg:col-start-2'}`}>
                         <h2 className='text-4xl font-title md:text-4xl lg:text-5xl text-gray-800 font-bold uppercase mb-6'>
                             {title2}
                         </h2>
