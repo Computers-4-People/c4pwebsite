a1e5ef6d8e21b89449eac4d9a8e6940fea386bcf
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-09-11 23:16:11 -0400

    made it so that header content doesn't overflow the space alotted to it

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -37,7 +37,7 @@ export default function Header(props) {
                 <button
                     key={index}
                     onClick={link.clickAction}
-                    className={`block w-full md:w-2/5 min-w-auto md:inline-block text-center md:text-xl animate-fade-up h-11 rounded-md md:px-7 
+                    className={`block w-full min-w-max md:w-2/5 min-w-auto md:inline-block text-center md:text-xl animate-fade-up h-11 rounded-md md:px-7 
                         ${index % 2 === 0 ? 'bg-c4p hover:bg-c4p-hover hover:text-white' : 'border border-white text-white hover:bg-white hover:text-black'}`}
                 >
                     {link.text}
@@ -51,7 +51,7 @@ export default function Header(props) {
             style={{
                 backgroundImage: `linear-gradient(to right, black 0%, rgba(0, 0, 0, 0.8) 30%, rgba(0, 0, 0, 0.5) 60%, transparent 100%),url('${bgImage}')`
             }}
-            className="bg-cover h-screen bg-center bg-fixed bg-no-repeat flex flex-col justify-end md:flex-row pb-10 md:items-center md:justify-start"
+            className="bg-cover min-h-max-content h-screen bg-center bg-fixed bg-no-repeat flex flex-col justify-end md:flex-row pb-10 md:items-center md:justify-start"
         >
             {/* <div className="px-4 sm:px-10 md:px-20 justify-items-stretch">
                 <h1 className='col-start-1 md:col-end-6 font-title text-6xl md:text-6xl lg:text-9xl mb-4'>
@@ -68,14 +68,14 @@ export default function Header(props) {
             <div className="px-4 sm:px-10 md:px-20 justify-items-stretch">
     {/* Wrapper for h1 that controls the width */}
     <div className="inline-block">
-        <h1 className='col-start-1 md:col-end-6 font-title text-6xl md:text-6xl lg:text-9xl mb-4'>
+        <h1 className='col-start-1 md:col-end-6 font-title text-6xl md:text-8xl 2xl:text-9xl mb-4'>
             <p className='text-c4p animate-fade-up'>{titlePart1}</p>
             <p className='text-white animate-fade-up'>{titlePart2}</p>
         </h1>
     </div>
 
-    <div className="col-start-1 md:col-end-5 animate-fade-up" style={{ maxWidth: '80%' }}>
-        <p className='text-white text-xl md:text-2xl mb-4'>{description}</p>
+    <div className="col-start-1 md:col-end-5 animate-fade-up" style={{ maxWidth: '70%' }}>
+        <p className='text-white text-xl 2xl:text-2xl mb-4'>{description}</p>
         <div className='flex flex-row space-x-5'>
             {links.map(renderLinkItem)}
         </div>
