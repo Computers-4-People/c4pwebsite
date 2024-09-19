b88b5f6a062ff640d5879c10cd4a33ae1a51c9e0
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-09-19 14:28:05 -0400

    made the width on the descriptions of headers smaller on medium sized screens and above

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -63,7 +63,7 @@ export default function Header(props) {
                     </h1>
                 </div>
                 <div className="animate-fade-up">
-                    <p className='text-white text-xl 2xl:text-2xl mb-4'>{description}</p>
+                    <p className='text-white text-xl 2xl:text-2xl mb-4 md:w-3/4'>{description}</p>
                     <div className='flex flex-col space-y-3 md:flex-row md:space-x-5 md:space-y-0 md:w-1/2'>
                         {links.map(renderLinkItem)}
                     </div>
