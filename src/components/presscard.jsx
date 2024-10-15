79d6a61dd77e0640bb8272f7f546b1bbd24f9987
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-10-15 13:19:57 -0400

    restricted title lines number to 2 for press page added ellipsis

--- src/components/presscard.jsx
+++ src/components/presscard.jsx
@@ -48,11 +48,11 @@ const PressCard = ({ pressItems }) => {
                         }}
                     >
                         {pressItems.map((item, index) => (
-                            <div key={index} className={`px-4 ${isMobile ? 'w-full' : 'w-1/3'}`}>
+                            <div key={index} className={`px-4 h-screen ${isMobile ? 'w-full' : 'w-1/3'}`}>
                                 <div className="bg-white shadow-lg rounded-lg overflow-hidden">
                                     <img src={item.image} alt={item.title} className="w-full h-64 object-cover" />
-                                    <div className="p-6">
-                                        <h3 className="text-2xl font-bold mb-2">{item.title}</h3>
+                                    <div className="p-6 h-64">
+                                        <h3 className="text-2xl font-bold mb-2 h-16 line-clamp-2 text-ellipsis">{item.title}</h3>
                                         <p className="text-gray-500 mb-4">{item.date}</p>
                                         <p className="text-gray-700 mb-6">{item.description}</p>
                                         <a href={item.link} className="inline-block bg-c4p text-white px-4 py-2 rounded-md hover:bg-c4p-hover">
