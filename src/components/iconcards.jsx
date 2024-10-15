3d0e66bf18db70c25bbb28f108059edc2d5e1d41
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-10-15 13:01:15 -0400

    made iconcards look more clickable

--- src/components/iconcards.jsx
+++ src/components/iconcards.jsx
@@ -6,9 +6,9 @@ const IconCards = ({ cards }) => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10 px-10 py-5">
             {cards.map((card, index) => (
-                <Link to={card.link} key={index} className="group block overflow-hidden rounded-2xl shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl">
+                <Link to={card.link} key={index} className="group block overflow-hidden pt-5 rounded-2xl shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl hover:bg-gray-300">
                     <img src={card.image} alt={card.alt} className="w-full mb-5 transition duration-300 ease-linear group-hover:opacity-75" />
-                    <div className="bg-white p-5 text-center font-bold">
+                    <div className="bg-c4p hover:bg-green-600 p-5 text-center font-bold">
                         {card.title}
                     </div>
                 </Link>
