73af88860f41c738a2a0ed61d4276d12f6d5510d
dylanzajac1 <143460154+dylanzajac1@users.noreply.github.com>
2024-11-23 14:24:50 -0500

    nonprofit application page done

--- src/homepage/nonprofitapplication.jsx
+++ src/homepage/nonprofitapplication.jsx
@@ -15,9 +15,8 @@ export default function Nonprofitapplication() {
            <div style={{
   backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.5)), url('/refurbished/refurbishedbackground.png')`,
   display: 'flex',
-  alignItems: 'center', // This aligns the iframe vertically
   justifyContent: 'center', // This aligns the iframe horizontally
-  height: '100vh',
+  height: '150vh',
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat'
@@ -26,8 +25,8 @@ export default function Nonprofitapplication() {
     className="pt-20"
     scrolling="no"
     aria-label='Hotspot Form'
-    style={{ width: '100%', height: '100%', border: 'none' }} // Make iframe fullscreen and remove border
-    src='https://forms.zohopublic.com/Computers4People/form/Hotspot2024Draft/formperma/O4b84XQEgAkGoxlKQL-tCYDf1kqU4nzORnvem_gy55M'
+    style={{ width: '100%', height: '100vh', border: 'none' }} // Make iframe fullscreen and remove border
+    src='https://forms.zohopublic.com/Computers4People/form/NonprofitApplication/formperma/yEPcvpgg60JZsEkbUysq0001f3mYS_Hnbur5-NZqoPo'
  ></iframe>
 </div>
            </div>
