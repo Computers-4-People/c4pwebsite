529dbf673a33e0ded1b86288dc2c89bfcc4cc984
Alen Ganopolsky <alenganopolsky@gmail.com>
2025-03-16 22:15:24 -0400

    modified email.js

--- api/email.js
+++ api/email.js
@@ -48,8 +48,8 @@ export default async function handler(req, res) {
 
 
     
-    await mg.messages.create(domainTwo, {
-      from: `Computers4People <mailgun@${domainTwo}>`,
+    await mg.messages.create('www.computers4people.org', {
+      from: "Mailgun Sandbox <postmaster@www.computers4people.org>",
       to: [decodedEmail],
       subject: 'Welcome to Computers4People!',
       text: `Your record ID is ${recordId}`,
