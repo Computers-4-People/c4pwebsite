bb316aa03c14f13f592723443f2db74efe7d1673
dylanzajac1 <143460154+dylanzajac1@users.noreply.github.com>
2025-04-20 22:24:29 -0400

    updated about stats

--- src/homepage/about.jsx
+++ src/homepage/about.jsx
@@ -77,9 +77,9 @@ export default function About() {
                 }} /> 
 <div className='bg-cover bg-black p-16 text-white'>
   <ul className='flex flex-col md:flex-row justify-evenly text-center'>
-    <li className='my-4 md:my-0 mx-5'><div className='text-6xl mb-3'>4,136</div><p className='text-3xl'>Computers Donated</p></li>
-    <li className='my-4 md:my-0 mx-5'><div className='text-6xl mb-3'>61,021</div><p className='text-3xl'>Pounds of e-waste Recycled</p></li>
-    <li className='my-4 md:my-0 mx-5'><div className='text-6xl mb-3'>215</div><p className='text-3xl'>Digital Skills Classes</p></li>
+    <li className='my-4 md:my-0 mx-5'><div className='text-6xl mb-3'>4,551</div><p className='text-3xl'>Computers Donated</p></li>
+    <li className='my-4 md:my-0 mx-5'><div className='text-6xl mb-3'>58.353</div><p className='text-3xl'>Pounds of e-waste Recycled</p></li>
+    <li className='my-4 md:my-0 mx-5'><div className='text-6xl mb-3'>219</div><p className='text-3xl'>Digital Skills Classes</p></li>
   </ul>
 </div>
             
