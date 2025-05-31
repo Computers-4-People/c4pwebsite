261997857cf7a0e39a70e66035a12eec7cc946fc
Alen Ganopolsky <alenganopolsky@gmail.com>
2025-05-31 17:16:50 -0400

    changes to frontend --- formatting, fixing button turning green, some optimization

--- src/components/portaldropdown.jsx
+++ src/components/portaldropdown.jsx
@@ -3,21 +3,28 @@ import PortalButton from './portalbutton';
 
 const PortalDropdown = ({type, applicantType}) => {
 
+
+    /// ??? ;0  
     
     let name = "";
+    let page = "";
 
     if (applicantType === 'Computer Donations') {
         name = 'Computer Donations';
+        page = 'Computer Donations';
     } else if (applicantType === 'Contacts') {
-        name = 'Computer Applications';
+        name = 'Computer Applications'; 
+        page = 'Computer Applications';
     }
+
+
 
     return (
         <div className="w-full md:w-64 space-y-2">
             
             <div className="flex flex-col gap-2">
                 <PortalButton type={type} page={"Champions"} onClick="/champions">Champion</PortalButton>
-                <PortalButton type={type} page={"Computer Donations"} onClick="/portal">{name}</PortalButton>
+                <PortalButton type={type} page={page} onClick="/portal">{name}</PortalButton>
             </div>
         </div>
     );
