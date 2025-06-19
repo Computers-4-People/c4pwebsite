22b8c51e32b6aecfcd81aea6194dd7d4b1257dd6
Dylan Zajac <dylanzajac@computers4people.org>
2025-06-19 15:30:30 -0400

    new vids for people

--- src/homepage/fundraiser/scottcook.jsx
+++ src/homepage/fundraiser/scottcook.jsx
@@ -51,7 +51,7 @@ export default function ScottCook() {
             <iframe
               width="360"
               height="640"
-              src="https://www.youtube.com/embed/WYfNMojvKYA"
+              src="https://www.youtube.com/embed/Xyi3Ui9exVU"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
